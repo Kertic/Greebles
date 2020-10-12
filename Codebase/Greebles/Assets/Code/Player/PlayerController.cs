941a5815cec796322af0033f2bebe2895e8c1198
Kertic <yougotbrennan@gmail.com>
2020-10-11 19:16:17 -0500

    Added a animated walking player

--- Codebase/Greebles/Assets/Code/Player/PlayerController.cs
+++ Codebase/Greebles/Assets/Code/Player/PlayerController.cs
@@ -6,6 +6,12 @@ namespace Code.Player
     public class PlayerController : MonoBehaviour, IServiceResolvable
     {
         private InputManager _inputManager;
+        private bool _isFacingRight;
+        private static readonly int _isWalking = Animator.StringToHash("isWalking");
+
+        [SerializeField] public float movementSpeed;
+        [SerializeField] public Animator _animator;
+        [SerializeField] public SpriteRenderer _sprite;
 
         public void ResolveServices()
         {
@@ -16,15 +22,40 @@ namespace Code.Player
         void Start()
         {
             ResolveServices();
+            _isFacingRight = true;
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (_inputManager.GetButton(InputManager.InputTypes.UP_TAP))
+            UpdateMotion();
+        }
+
+        private void UpdateMotion()
+        {
+            void UpdateDirection(InputManager.InputTypes type, Vector3 movementDirection)
             {
-                Debug.Log("Up tapped");
+                if (!_inputManager.GetButton(type)) return;
+                gameObject.transform.position += movementDirection * movementSpeed;
+                _animator.SetBool(_isWalking, true);
             }
+
+            _animator.SetBool(_isWalking, false);
+            UpdateDirection(InputManager.InputTypes.UP_HELD, Vector3.up);
+            UpdateDirection(InputManager.InputTypes.DOWN_HELD, Vector3.down);
+            UpdateDirection(InputManager.InputTypes.LEFT_HELD, Vector3.left);
+            UpdateDirection(InputManager.InputTypes.RIGHT_HELD, Vector3.right);
+            if (_inputManager.GetButton(InputManager.InputTypes.LEFT_HELD))
+            {
+                _isFacingRight = false;
+            }
+
+            if (_inputManager.GetButton(InputManager.InputTypes.RIGHT_HELD))
+            {
+                _isFacingRight = true;
+            }
+
+            _sprite.flipX = !_isFacingRight;
         }
     }
 }
